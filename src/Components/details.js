c265b4d1cbafc9ef9e930a9f58a279d41ee04939
Kanishk Singh <kanishk.singh198@gmail.com>
2022-10-15 18:35:56 +0530

    separate components in different files

--- src/Components/details.js
+++ src/Components/details.js
@@ -1,23 +1,24 @@
 const Details = ({ todoList, deleteTodo }) => {
 	return (
 		<div>
-			{todoList.map((todo) => {
-				return (
-					<div key={todo}>
-						<h2>{todo.title}</h2>
-						<h3>{todo.details}</h3>
-						{/* <h3>{todo.id}</h3> */}
-						<button
-							onClick={(e) => {
-								deleteodo(todo.id);
-							}}
-						>
-							Done
-						</button>
-						<hr></hr>
-					</div>
-				);
-			})}
+			{todoList
+				? todoList.map((todo) => {
+						return (
+							<div key={todo.id}>
+								<h2>{todo.title}</h2>
+								<h3>{todo.details}</h3>
+								<button
+									onClick={(e) => {
+										deleteTodo(todo.id);
+									}}
+								>
+									Done
+								</button>
+								<hr></hr>
+							</div>
+						);
+				  })
+				: null}
 		</div>
 	);
 };
--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
-// import Form from "./Components/Form";
-// import Details from "./Components/details";
+import Form from "./Components/Form";
+import Details from "./Components/details";
 import { v4 as uuid } from "uuid";
 import ErrorBoundary from "./Components/ErrorBoundary";
+
+if (!localStorage.getItem("todoList")) {
+	localStorage.setItem("todoList", JSON.stringify([]));
+}
 
 function Todo() {
 	const [todoList, setTodoList] = useState(
@@ -15,12 +19,14 @@ function Todo() {
 		const id = uuid();
 		let newTodoList = [];
 
-		if (todoList) {
-			newTodoList = [...todoList, { title, details, id }];
-		} else {
-			newTodoList = [{ title, details, id }];
+		if (title) {
+			if (todoList) {
+				newTodoList = [...todoList, { title, details, id }];
+			} else {
+				newTodoList = [{ title, details, id }];
+			}
+			setTodoList(newTodoList);
 		}
-		setTodoList(newTodoList);
 	};
 
 	const deleteTodo = (id) => {
@@ -36,62 +42,13 @@ function Todo() {
 		}
 	}, [todoList]);
 
-	// console.log(todoList);
-	// console.log(localStorage.getItem("todoList"));
-
 	return (
 		<div>
 			<h1>ToDo List</h1>
-			<div>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						const formData = new FormData(e.target);
-						// console.log({formData})
-						addTodo(formData);
-						e.target.reset();
-					}}
-				>
-					<label htmlFor="title">
-						{" "}
-						title &nbsp;
-						<input type="text" name="title" id="title"></input>
-						&nbsp;
-					</label>
-					<label htmlFor="details">
-						{" "}
-						Details &nbsp;
-						<input type="text" name="details" id="details"></input>
-						&nbsp;
-					</label>
-					<button>Submit</button>
-				</form>
-			</div>
-			{/* <ErrorBoundary>
-				<Form addTodo />
-			</ErrorBoundary> */}
-			<div>
-				{todoList
-					? todoList.map((todo) => {
-							return (
-								<div key={todo.id}>
-									<h2>{todo.title}</h2>
-									<h3>{todo.details}</h3>
-									{/* <h3>{todo.id}</h3> */}
-									<button
-										onClick={(e) => {
-											deleteTodo(todo.id);
-										}}
-									>
-										Done
-									</button>
-									<hr></hr>
-								</div>
-							);
-					  })
-					: null}
-			</div>
-			{/* <Details todoList={todoList} removeTodo /> */}
+			<ErrorBoundary>
+				<Form addTodo={addTodo} />
+			</ErrorBoundary>
+			<Details todoList={todoList} deleteTodo={deleteTodo} />
 		</div>
 	);
 }
